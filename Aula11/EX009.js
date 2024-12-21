7357c4d5c519367cf1959571bb80ffbfbab980f2
Erick Caetano <erick.caetano1200@gmail.com>
2024-12-20 23:29:07 -0300

    Aula sobre condicionais em Javascript

---
+++ Aula11/EX009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil';
+
+console.log(`Vivendo em Paìs ${pais}`);
+
+if(pais == 'Brasil'){
+    console.log("Brasileiro")
+}else{
+    console.log("Estrangeiro")
+}
---
+++ Aula11/ex008.js
@@ -0,0 +1,9 @@
+console.log("O console ta rodando.....");
+
+var vel = 60 
+console.log(`A velocidade do seu carro é ${vel}km/h `);
+
+if(vel > 60){  //Condição SImples
+    console.log("Voce foi Multado!!!")
+}
+console.log("Dirija sempre usando o cinto de segurança")
