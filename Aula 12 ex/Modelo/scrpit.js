28ac1f9201b992bd54c6c7e0a312bccd4b534676
Erick Caetano <erick.caetano1200@gmail.com>
2025-01-01 19:05:33 -0300

    Estrutura de Repetição em Javascript

--- Aula 12 ex/Modelo/scrpit.js
+++ Aula 12 ex/Modelo/scrpit.js
@@ -1,1 +1,63 @@
-alert('ola!')
+function verificador(){
+
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  
+  // condiçaõ para verificar erro 
+if( fano.value.length == 0 || fano.value > ano ){
+    window.alert("Verifique os dados e tente novamente")
+}else{
+   //aqui calculo as sentenças como genero e idade
+   var fsex = document.getElementsByName('radsex')
+   var idade = ano - Number(fano.value)
+   var img = document.createElement('img')
+     img.setAttribute('id' , 'foto')
+
+
+   //Verificando o genero 
+var genero = ''
+if(fsex[0].checked){
+    genero = 'Homem'
+
+     if( idade <= 13 && idade >= 0 ){
+        //Criança
+        img.setAttribute('src' , 'menino.jpg')
+     }else if ( idade <= 55){
+        //Adulto
+        img.setAttribute('src' , ' Homem Adulto.jpg')
+
+     }else{
+        //idoso
+        img.setAttribute('src' , ' Homem Idoso.jpg')
+
+
+        
+     }  
+} else if ( fsex[1].checked){
+    genero = "Mulher"
+
+    if( idade <= 13 && idade >= 0 ){
+        //Criança
+        img.setAttribute('src' , 'menina.jpg')
+
+     }else if ( idade <= 55){
+        //Adulto
+        img.setAttribute('src' , ' Mulher Adulta.jpg')
+
+     }else{
+        //idoso
+        img.setAttribute('src' , ' Mulher Idosa.jpg')
+
+     }    
+    
+}
+
+
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    //aqui abaixo adiciono a imagem como resultado para exibição
+    res.appendChild(img)
+}
+}   
---
+++ Aula13/node_b05d13a50f0e1.tmp.js
@@ -0,0 +1,24 @@
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+//console.log('Jeito dificil')
+
+//Estrutura de Repetição
+
+var c = 1
+/*
+while (c < 8){
+    console.log(`Passo ${c}`)
+    c++
+}  */
+
+// executa primeiro depois verifica
+
+do {
+
+    console.log(`Passo ${c}`)
+
+}while(c < 6)
